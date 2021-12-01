9f0e50e21c35d72fcee547cdee55b04df1b9406e
BlockCat <zinoonomiwo@gmail.com>
2021-12-01 19:53:30 +0100

    to benches

--- examples/day_01.rs
+++ examples/day_01.rs
@@ -1,9 +1,3 @@
-#![feature(test)]
-#![feature(portable_simd)]
-
-use std::simd::u32x16;
-extern crate test;
-
 pub fn main() {
     let numbers: Vec<isize> = include_str!("../input/day01.txt")
         .lines()
@@ -20,38 +14,4 @@ fn exercise<const N: usize>(count: &[isize]) -> usize {
         .windows(N)
         .filter(|window| window[N - 1] > window[0])
         .count()
-}
-
-
-// #[cfg(test)]
-mod benches {
-    use crate::{*, test::Bencher};
-
-    #[bench]
-    fn input(b: &mut Bencher) {
-        b.iter(|| {
-            include_str!("../input/day01.txt")
-                .lines()
-                .map(|x| x.parse::<isize>().unwrap())
-                .collect::<Vec<_>>()
-        });
-    }
-
-    #[bench]
-    fn ex1_normal(b: &mut Bencher) {
-        let numbers: Vec<isize> = include_str!("../input/day01.txt")
-            .lines()
-            .map(|x| x.parse::<isize>().unwrap())
-            .collect();
-        b.iter(|| exercise::<2>(&numbers));
-    }
-
-    #[bench]
-    fn ex2_normal(b: &mut Bencher) {
-        let numbers: Vec<isize> = include_str!("../input/day01.txt")
-            .lines()
-            .map(|x| x.parse::<isize>().unwrap())
-            .collect();
-        b.iter(|| exercise::<4>(&numbers));
-    }
 }
