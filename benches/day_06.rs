eae8e8417f1867700879bd999ec8278904eb508a
BlockCat <zinoonomiwo@gmail.com>
2021-12-06 06:21:07 +0100

    day06

---
+++ benches/day_06.rs
@@ -0,0 +1,27 @@
+#![feature(test)]
+extern crate test;
+use self::test::Bencher;
+
+include!("../examples/day_06.rs");
+
+#[bench]
+fn d06_input(b: &mut Bencher) {
+    b.iter(|| parse_input(include_str!("../input/day06.txt")));
+}
+
+#[bench]
+fn d06_ex1_normal(b: &mut Bencher) {
+    let input = parse_input(include_str!("../input/day06.txt"));
+    b.iter(|| exercise_n::<80>(&input));
+}
+
+#[bench]
+fn d06_ex2_normal(b: &mut Bencher) {
+    let input = parse_input(include_str!("../input/day06.txt"));
+    b.iter(|| exercise_n::<256>(&input));
+}
+
+// Version 1
+// test d06_ex1_normal ... bench:      44,315 ns/iter (+/- 385)
+// test d06_ex2_normal ... bench:     133,492 ns/iter (+/- 1,350)
+// test d06_input      ... bench:       5,539 ns/iter (+/- 794)
--- examples/day_06.rs
+++ examples/day_06.rs
@@ -1,3 +1,40 @@
+use std::collections::HashMap;
+
+type Input = Vec<usize>;
+
 pub fn main() {
-    
+    let input = parse_input(include_str!("../input/day06.txt"));
+
+    println!("Ex1: {}", exercise_n::<80>(&input));
+    println!("Ex2: {}", exercise_n::<256>(&input));
 }
+
+fn parse_input(input: &str) -> Input {
+    input.split(',').map(|x| x.parse().unwrap()).collect()
+}
+
+fn exercise_n<const N: usize>(lines: &Input) -> usize {
+    let mut group = HashMap::with_capacity(9);
+
+    for fish in lines {
+        *group.entry(*fish).or_insert(0usize) += 1;
+    }
+
+    for _ in 0..N {
+        let mut ng = HashMap::with_capacity(9);
+
+        for (days, amount) in group {
+            if days == 0 {
+                *ng.entry(6).or_insert(0) += amount;
+                *ng.entry(8).or_insert(0) += amount;
+            } else {
+                *ng.entry(days - 1).or_insert(0) += amount;
+            }
+        }
+        group = ng;
+    }
+
+    group.values().sum()
+}
+
+// fn exercise_2(lines: &Input) -> usize {}
