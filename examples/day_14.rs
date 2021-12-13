f8c74c8cd9021d115b2c1e1761af376afeb62a65
BlockCat <zinoonomiwo@gmail.com>
2021-12-13 08:34:21 +0100

    d12 d13

--- examples/day_14.rs
+++ examples/day_14.rs
@@ -1,3 +1,20 @@
+use std::ops::Index;
+
+type Input = Vec<usize>;
 pub fn main() {
-    
+    let input = parse_input(include_str!("../input/test.txt"));
+    println!("Ex1: {}", exercise_1(&input));
+    println!("Ex2: {}", exercise_2(&input));
+}
+
+fn parse_input(input: &str) -> Input {
+    input.lines().map(|line| {
+
+    }).collect()
+}
+
+fn exercise_1(input: &Input) -> usize {}
+
+fn exercise_2(input: &Input) -> usize {
+    0
 }
